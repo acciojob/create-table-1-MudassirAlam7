4e597ed6607b4b06fb7b3a2b4b4eb5135a5d1c63
MudassirAlam7 <mudassiralam7@gmail.com>
2024-12-16 22:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+let btn = document.querySelector('input')
+let table = document.querySelector('#sampleTable')
+
+
+let count = 1
 function insert_Row() {
-    //Write your code here
+  let newRow = document.createElement('tr')
+            let cell1 = document.createElement('td')
+            let cell2 = document.createElement('td')
+            cell1.textContent = `New Cell${count}`
+            cell2.textContent = `New Cell${count}`
+            newRow.appendChild(cell1)
+            newRow.appendChild(cell2)
+
+	let firstrow = table.firstChild;
+    table.insertBefore(newRow, firstrow)
+
+	
+            count++  
+	
   
   
 }
