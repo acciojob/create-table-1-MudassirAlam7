a0f4f0b059690926e4dc9a3468856519972feb3c
MudassirAlam7 <mudassiralam7@gmail.com>
2024-12-28 09:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let btn = document.querySelector('input')
 let table = document.querySelector('#sampleTable')
 
 
-let count = 1
+// let count = 1
 function insert_Row() {
   let newRow = document.createElement('tr')
             let cell1 = document.createElement('td')
             let cell2 = document.createElement('td')
-            cell1.textContent = `New Cell${count}`
-            cell2.textContent = `New Cell${count}`
+            cell1.textContent = `New Cell2`
+            cell2.textContent = `New Cell1`
             newRow.appendChild(cell1)
             newRow.appendChild(cell2)
 
